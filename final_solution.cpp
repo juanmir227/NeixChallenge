2090858e3f033491df657cf9a8181d23ffbe8072
Juan Cruz Miranda <juanmir227@gmail.com>
2024-03-05 20:35:19 -0300

    agregue comienzo de calculo de black scholes

--- final_solution.cpp
+++ final_solution.cpp
@@ -3,8 +3,7 @@
 
 
 
-double PI()
-{
+double PI() {
     return M_PI;
 }
 
@@ -13,30 +12,17 @@ double N(double x) {
     return 0.5 * (1.0 + erf(x / sqrt(2.0)));
 }
 
-
+// Función para el cálculo de la normal
 double F(double x) {
     double factor = 1.0/sqrt(2.0*PI());
     double e = exp(-pow(x, 2)/2.0);
     return factor*e;
 }
 
-double D1(double S, double K, double r, double q, double v, double t) {
-    double top = log(S / K) + (r - q + 0.5 * pow(v, 2)) * t;
+double D1(double S, double K, double r, double v, double t) {
+    double top = log(S / K) + (r + 0.5 * pow(v, 2)) * t;
     double bottom = v * sqrt(t);
     return top / bottom;
-}
-
-double BlackScholes(double S, double K, double r, double q, double v, double t, int optype)
-{
-    double d1 = D1(S, K, r, q, v, t);
-    double d2 = D2(d1, v, t);
-    double option_price = 0;
-    if(optype == 0){
-        option_price = S*exp(-q*t)*N(d1) - K*exp(-r*t)*N(d2);
-    } else {
-        option_price = K*exp(-r*t)*N(-d2) - S*exp(-q*t)*N(-d1);
-    }
-    return option_price;
 }
 
 
@@ -44,29 +30,59 @@ double D2(double d1, double v, double t) {
     return d1 - v*sqrt(t);
 }
 
-// double Vega(double S, double K, double r, double q, double v, double t)
-// {
-//     double d1 = D1(S, K, r, q, v, t);
-//     return (1.0/100.0)*S*N(d1)*sqrt(t);
-// }
-// change in the function
+
+// double BlackScholes(double S, double K, double r, double v, double t) {
+double BlackScholes(double d1, double d2, double S, double K, double r, double t) {
+    // double d1 = D1(S, K, r, v, t);
+    // double d2 = D2(d1, v, t);
+    double option_price = 0;
+    option_price = S*N(d1) - K*exp(-r*t)*N(d2);
+    return option_price;
+}
 
 double Vega(double S, double d1, double t) {
-    return (1.0/100.0)*S*N(d1)*sqrt(t);
+    return S*F(d1)*sqrt(t);
 }
 
 
 int main() {
-    double S = 297.653;
+    double S = 1210.5;
     double K = 1033;
+    double C0 = 301.84900000000005;
     double r = 0.9;
-    double q = 0.0147;
-    // double v = 0.11; debo proveer valor inicial
-    double t = 0.11575342465753424;
-    double result1 = D1(S, K, r, q, 0.11, t);
-    double result2 = D2(result1, 0.11, t);
-    
-    std::cout << "D1 Result: " << result1 << std::endl;
-    std::cout << "D2 Result: " << result2 << std::endl;
+    double t = 0.1157496194824962;
+    double v = 0.3; // mi propuesta de vatilidad
+    double abstol = 1e-6;
+    double epsilon = 1.0;
+    int i = 0;
+    int max_iter = 1e3;
+
+
+    while (epsilon > abstol) {
+        if (i > max_iter) {
+            std::cout << "El programa ha alcanzado el máximo de iteraciones "<< std::endl;
+            break;
+        }
+
+        i++;
+        double orig = v;
+        double d1 = D1(S, K, r, v, t);
+        double d2 = D2(d1, v, t);
+        double function_value = BlackScholes(d1, d2, S, K, r, t) - C0;
+        double vega = S * F(d1) * sqrt(t);
+        v = -function_value / vega + v;
+        epsilon = std::abs(function_value);
+    }
+
+    std::cout << "Implied volatility = " << v << std::endl;
+    std::cout << "Code required " << i << " iterations." << std::endl;
+
     return 0;
+
+
+    
+    // double result1 = D1(S, K, r, v, t);
+    // double result2 = D2(result1, v, t);
+    // double vega = Vega(S,result1,t);
+    // double optionprice = BlackScholes(S, K, r,v, t);
 }
