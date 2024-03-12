363ae6d852b3dc34319d1ddb218f07c422d151de
Juan Cruz Miranda <juanmir227@gmail.com>
2024-03-12 18:43:46 -0300

    added final comments, done

--- generateGraph.py
+++ generateGraph.py
@@ -3,38 +3,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates
 
-# Cargo los archivos a utilizar, los resultados del programa computeVols.cpp y además la tira temporal obtenida al procesar
-# los datos.
+# Se cargan los archivos a utilizar: los resultados del programa computeVols.cpp y las fechas.
 implied = pd.read_csv('impliedVols.csv')
 realized = pd.read_csv('realizedVols.csv')
 dates = pd.read_csv('dates.csv')
 dates.created_at = pd.to_datetime(dates.created_at)
-times = pd.read_csv('processedData.csv', sep = ';')
-times['times'] = times.timeToMaturity.apply(lambda x: x*365)
-times = times['times'].tolist()
 
-# Dado que para la realizada tengo un punto menos genero dos tiras temporales.
-# impliedDates = times
-# realizedDates = times[1:]
+# Dado que para la volatilidad realizada se obtuvo un punto menos, se generan dos tiras temporales para mayor claridad.
 impliedDates = dates
 realizedDates = dates[1:]
 
-# Genero una figura que contiene los resultados obtenidos para las volatilidades. Esta contiene tres gráficos.
-# Arriba los resultados obtenidos para ambas volatilidades superpuestas. Abajo ambas volatilidades por separado para
-# mayor claridad.
+# Se genera una figura que contiene los resultados obtenidos para las volatilidades. Esta contiene tres gráficos. Arriba los resultados obtenidos para ambas volatilidades superpuestas. Abajo ambas volatilidades por separado para mayor claridad.
 fig = plt.figure(figsize = (16,10))
 plt.subplot(2, 1, 1)
 plt.plot(impliedDates, implied*100, color = 'blue', label = 'Volatilidad implícita')
 plt.plot(realizedDates, realized*100, color = 'orange', alpha = 0.6, label = 'Volatilidad realizada')
-# ax = plt.gca()
-# ax.invert_xaxis()
 plt.xlabel('Fecha', fontsize = 14)
 plt.ylabel('Volatilidad (%)', fontsize = 14)
 plt.legend(fontsize = 13)
 plt.grid(alpha = 0.5)
 plt.title('Comparación de volatilidad implícita y realizada', fontsize = 20)
-
-
 
 plt.subplot(2, 2, 3)
 plt.plot(impliedDates, implied*100, color='blue')
@@ -42,8 +30,6 @@ plt.title('Volatilidad implícita')
 plt.xlabel('Fecha')
 plt.ylabel('Volatilidad (%)')
 plt.grid(alpha = 0.5)
-# ax = plt.gca()
-# ax.invert_xaxis()
     
 plt.subplot(2, 2, 4)
 plt.plot(realizedDates, realized*100, color='red')
@@ -51,10 +37,8 @@ plt.title('Volatilidad realizada')
 plt.xlabel('Fecha')
 plt.ylabel('Volatilidad (%)')
 plt.grid(alpha = 0.5)
-# ax = plt.gca()
-# ax.invert_xaxis()
 
 plt.tight_layout()
 
-# #Guardo la figura en formato pdf para insertar en el README.
+# #Guardo la figura en formato png para insertar en el README.
 plt.savefig('graphs/volatilityComparison.png')
--- noSolution.py
+++ noSolution.py
@@ -3,29 +3,34 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from scipy.stats import norm
 from scipy.interpolate import interpolate
+
+# Cargo los datos.
 data = pd.read_csv('processedData.csv', sep = ';')
 
-
+# Esta función calcula d1 y d2 de la fórmula de Black-Scholes.
 def d(sigma, S, K, r, t):
     d1 = 1 / (sigma * np.sqrt(t)) * ( np.log(S/K) + (r + sigma**2/2) * t)
     d2 = d1 - sigma * np.sqrt(t)
     return d1, d2
-
+# Esta función calcula el precio de la opción con Black-Scholes.
 def call_price(sigma, S, K, r, t, d1, d2):
     C = norm.cdf(d1) * S - norm.cdf(d2) * K * np.exp(-r * t)
     return C
 
-
+# Esta función llama a las funciones anteriormente definidas.
 def f(sigma, S, K, r, t, C0):
     d1, d2 = d(sigma, S, K, r, t)
     C = call_price(sigma, S, K, r, t, d1, d2) - C0
     return C
 
+# Se definen constantes utilizadas para el cálculo las cuales fueron tomadas de los datos.
 S = 1166.5
 K = 1033
 t = 0.022515220700152206
 r = 0.9
 C0 = 145.4505
+
+# Se realiza un gráfico de la fórmula de Black-Scholes en función del valor de la volatilidad Sigma. Y se observa que no existe una raíz para los datos propuestos.
 plt.figure(figsize = (16,10))
 sigma = np.linspace(0.001, 1,1000)
 plt.plot(sigma, f(sigma, S, K, r, t, C0), 'b')
@@ -35,4 +40,5 @@ plt.xlabel('Valor de $\sigma$', fontsize = 14)
 plt.ylabel('BlackScholes() - data', fontsize =14)
 plt.grid(alpha = 0.5)
 plt.tight_layout()
+# Se guarda el gráfico.
 plt.savefig('graphs/noSolution.png')
