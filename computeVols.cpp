dadd5802894549468f19f5896ff719021e307d3b
Juan Cruz Miranda <juanmir227@gmail.com>
2024-03-12 18:16:14 -0300

    adding comments

--- computeVols.cpp
+++ computeVols.cpp
@@ -5,12 +5,14 @@
 #include <vector>
 #include <string>
 
+// Genero structs para mayor orden.
+// Con vectores a utilizar que se encuentran dentro del CSV.
 struct CSVData {
     std::vector<double> price;
     std::vector<double> underPrice;
     std::vector<double> timeToMaturity;
 };
-
+// Con constantes que no cambiarán durante el proceso.
 struct Constantes {
     const double K = 1033;
     const double r = 0.9;
@@ -18,29 +20,29 @@ struct Constantes {
     const double epsilon = 1.0;
     const int max_iter = 1e3;
 };
-
+// Creo una función que toma un string, genera una instancia del struct CSVData y llena sus vectores con el
+// contenido del Csv.
 CSVData readCSV(const std::string& filename) {
     CSVData data;
 
     try {
-        // Open the CSV file
+
         std::ifstream file(filename);
 
-        // Check if the file is open
+        // Checkeo si efectivamente se abrió el archivo correctamente. Donde en caso de fallar, se detendrá el proceso.
         if (!file.is_open()) {
             throw std::ios_base::failure("Error opening file.");
         }
 
         std::string line;
-        // Read the header line (assuming the first line is a header)
+        // Guardo el header
         std::getline(file, line);
 
-        // Read the data lines
         while (std::getline(file, line)) {
             std::stringstream ss(line);
             std::string token;
 
-            // Read each column using stringstream
+            // Guardo los datos en los vectores.
             std::getline(ss, token, ';');
             data.price.push_back(std::stod(token));
 
@@ -51,88 +53,95 @@ CSVData readCSV(const std::string& filename) {
             data.timeToMaturity.push_back(std::stod(token));
         }
 
-        // Close the file
+        // Cierro el archivo
         file.close();
     } catch (const std::exception& e) {
         std::cerr << "Exception: " << e.what() << std::endl;
-        // Clear data if an exception occurs
+        // Limpio data si ocurre una excepción.
         data = CSVData();
     }
 
     return data;
 }
 
+// Esta función toma un vector y un string y guarda el contenido del vector en un archivo Csv.
 void saveToCSV(const std::vector<double>& vec, const std::string& filename) {
     std::ofstream file(filename);
-
+    // Chekeo que se pudo abrir efectivamente. En caso de fallar se printea un error.
     if (!file.is_open()) {
         std::cerr << "Error opening file." << std::endl;
         return;
     }
 
-    // Write header
+    
     file << "Volatility" << std::endl;
 
-    // Write data
+    
     for (size_t i = 0; i < vec.size(); ++i) {
         file << vec[i] << std::endl;
     }
 
     file.close();
 }
-
+// Función que calcula la constante Pi.
 double PI() {
     return M_PI;
 }
 
-// Agrego una función para el cálculo de la CDF que voy a necesitar para calcular d1 y d2 de Black-Scholes.
+// Esta función calcula la CDF que voy a necesitar para calcular d1 y d2 de Black-Scholes.
 double N(double x) {
     return 0.5 * (1.0 + erf(x / sqrt(2.0)));
 }
 
-// Función para el cálculo de la normal
+// Esta función calcula la normal que voy a necesitar para la función Vega, o derivada de Black-Scholes.
 double F(double x) {
     double factor = 1.0/sqrt(2.0*PI());
     double e = exp(-pow(x, 2)/2.0);
     return factor*e;
 }
 
-// Explicar que decidi separar las funciones para mayor claridad aunque pago con mas lineas de codigo.
+// Esta función calcula d1, el argumento de la primer parte de la fórmula de Black-Scholes.
 double D1(double S, double K, double r, double v, double t) {
     double top = log(S / K) + (r + 0.5 * pow(v, 2)) * t;
     double bottom = v * sqrt(t);
     return top / bottom;
 }
-
+// Esta función calcula d2, el argumento de la segunda parte de al fórmula de Black-Scholes.
 double D2(double d1, double v, double t) {
     return d1 - v*sqrt(t);
 }
 
-// double BlackScholes(double S, double K, double r, double v, double t) {
+// Esta función obtiene el valor del precio de la opción usando la fórmula de Black-Scholes.
 double BlackScholes(double d1, double d2, double S, double K, double r, double t) {
     double option_price = 0;
     option_price = S*N(d1) - K*exp(-r*t)*N(d2);
     return option_price;
 }
 
+// Esta función realiza el cálculo del valor de la función Vega la cual es la derivada en función de sigma de la fórmula de Black-Scholes.
 double Vega(double S, double d1, double t) {
     return S*F(d1)*sqrt(t);
 }
 
+// Esta función calcula la volatilidad implícita para cada uno de los puntos de mis datos.
 double ImpliedVolatility(double epsilon, double abstol, double max_iter, double S,double K,double r,double v,double t,double C0){
+
     int i = 0;
     while (epsilon > abstol) {
+        // En caso de superar las 1000 iteraciones sin converger, se romperá el loop. 
         if (i > max_iter) {
             std::cout << "El programa ha alcanzado el máximo de iteraciones "<< std::endl;
             break;
         }
         i++;
+        // utilizando un método de root finding llamado newton-raphson
         double d1 = D1(S, K, r, v, t);
         double d2 = D2(d1, v, t);
         double estimation = BlackScholes(d1, d2, S, K, r, t);
         double function_value = estimation - C0;
         double Vega = S * F(d1) * sqrt(t);
         v = -function_value / Vega + v;
+
         epsilon = std::abs(function_value);
     }
     return v;
